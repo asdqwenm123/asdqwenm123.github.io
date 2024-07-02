37d5d03f13a1dda760f474f43218271c9b3b0bae
하승우 <142319950+asdqwenm123@users.noreply.github.com>
2024-07-02 20:46:34 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function main() {
   const $3_per_sc = isNaN(parseFloat(document.getElementById("3_per_sc").value)) ? 0 : parseFloat(document.getElementById("3_per_sc").value);
   const $3_per_pe = isNaN(parseFloat(document.getElementById("3_per_pe").value)) ? 0 : parseFloat(document.getElementById("3_per_pe").value);
 
-  const last = mid_ex_sc * mid_ex_per / 100 + last_ex_sc * last_ex_per / 100 + $1_per_sc * $1_per_pe / 100 + $2_per_sc * $2_per_pe / 100 + $3_per_sc * $3_per_pe / 100;
-  const real_last = last.toFixed(0);
+  let last = mid_ex_sc * mid_ex_per / 100 + last_ex_sc * last_ex_per / 100 + $1_per_sc * $1_per_pe / 100 + $2_per_sc * $2_per_pe / 100 + $3_per_sc * $3_per_pe / 100;
+  let real_last = last.toFixed(0);
   let ABCDE;
   if (real_last >= 90) {
     ABCDE = "A";
@@ -41,6 +41,45 @@ function main() {
     "$3_per_pe": $3_per_pe,
   }
 
+  let A, B, C, D/*, E*/;
+  let a = false
+
+  if (last_ex_sc === 0) {
+    for (let i = 0; i <= 100; i++) {
+      let vir_last = mid_ex_sc * mid_ex_per / 100 + i * last_ex_per / 100 + $1_per_sc * $1_per_pe / 100 + $2_per_sc * $2_per_pe / 100 + $3_per_sc * $3_per_pe / 100;
+      let vir_real = vir_last.toFixed(0);
+
+      if (vir_real >= 90 && A === undefined) {
+        A = i;
+        a = true
+      }
+      if (vir_real >= 80 && B === undefined) {
+        B = i;
+        a = true
+      }
+      if (vir_real >= 70 && C === undefined) {
+        C = i;
+        a = true
+      }
+      if (vir_real >= 60 && D === undefined) {
+        D = i;
+        a = true
+      }
+      // if (vir_real >= 50 && vir_real > real_last && E === undefined) {
+      //   E = i;
+      //   a = true
+      // }
+    }
+    // console.log(A, B, C, D/*, E*/);
+    document.getElementById("last").innerHTML = "";
+    document.getElementById("expect").innerHTML = (a ? "최소..." : "아쉽게도 E밖에 계산이 안되었어요.") + (!A ? "" : " A: " + A) + (!B ? "" : ", B: " + B) + (!C ? "" : ", C: " + C) + (!D ? "" : ", D: " + D)/* + (E === undefined ? "" : ", E: " + E) */+ (a ? "점 이상!" : "");
+  } else {
+    document.getElementById("expect").innerHTML = ""
+    document.getElementById("last").innerHTML = real_last + "(" + ABCDE + ")";
+
+  }
+
+
   fetch('https://asdqwenm123.mcv.kr/score', {
     method: 'POST',
     headers: {
@@ -49,5 +88,5 @@ function main() {
     body: JSON.stringify(js)
   });
 
-  document.getElementById("last").innerHTML = real_last + "(" + ABCDE + ")";
+
 }
